bc6395202cc8aa5462ba362218c66bf9250f716d
Lee Peters <lpeters0023@gmail.com>
2020-02-15 13:12:58 -0500

    optimize game flow

--- index3.js
+++ index3.js
@@ -96,33 +96,40 @@ async function start() {
     else if (answer == 'take sign') {
       console.log ('That would be selfish. How will other students find their way?')
     } 
-    else if (answer == 'open door' || 'open') { 
-      console.log ('The door is locked. There is a keypad on the door handle.')
-      start()
-      if (answer.includes('12345')) {
+    else if (answer.includes('12345')) {
         console.log('Success! The door opens. You enter the foyer and the door\nshuts behind you');
         enterState('foyer')
         console.log(rooms[currentRoom].welcomeMessage)
-      } else if (answer.includes('seven days') || answer.includes('take')) {
-        console.log ('You pick up the paper and leaf through it looking for comics\nand ignoring the articles, just like everbody else does');
-        player.inventory.push('seven days')
-      } else if (answer.includes('drop') || answer.includes('leaves')) {
-        console.log (`You toss the "Seven Days" to the ground.`);
-        player.inventory.pop()
       } 
+
+    else if (answer.includes('take seven days')) {
+      console.log ('You pick up the paper and leaf through it looking for comics\nand ignoring the articles, just like everbody else does. What would you like to do now?');
+      player.inventory.push('seven days')
     } 
-    else if (answer.includes('stairs')) {
-      enterState('stairs')
+    //else if (answer.includes('drop') || answer.includes('leave')) {
+      //console.log (`You toss the "Seven Days" to the ground.`);
+      //player.inventory.pop()
+      //start()
+    //}
+    else if (answer.includes('stair') || answer.includes('up')) {
+      enterState('stairway')
+      console.log(rooms[currentRoom].welcomeMessage)
+      start()
+    }
+
+    else if (answer.includes('hall')) {
+      enterState('hallway')
       console.log(rooms[currentRoom].welcomeMessage);
     }
+
     else if (answer.includes('kitchen')) {
       enterState('kitchen')
       console.log(rooms[currentRoom].welcomeMessage)
       if (answer.includes('tea')) {
         player.inventory.push('tea')
-      } else { start() }
-    }
-    else if (answer.includes('classroom')) {
+      } 
+
+    else if (answer.includes('class')) {
       enterState('classroom')
       console.log(rooms[currentRoom].welcomeMessage)
       if (answer.includes('leave')) {
@@ -139,7 +146,7 @@ async function start() {
     showInventory()
     start()
     }
-  answer = await ask('>_');
+  //answer = await ask('What would you like to do? \n >_');
   }
    process.exit();
 }
