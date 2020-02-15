b8c6a9205cf7714cce082de6594c6c7cd7bf10d8
Lee Peters <lpeters0023@gmail.com>
2020-02-15 12:34:15 -0500

    nesting the if else statements

--- index3.js
+++ index3.js
@@ -10,6 +10,7 @@ function ask(questionText) {
 // | - - - starter code - do not change above - - - |
 let currentRoom = "outside";
 
+// player object 
 let player = {
   currentState: currentRoom, 
   inventory: []
@@ -28,28 +29,28 @@ let rooms = {
               (If you ever want to exit, just type -     exit   )`
             },
           
-  'sign':   {canChangeTo: ['foyer'],
+  'sign':   {canChangeTo: ['foyer', 'outside'],
              welcomeMessage: `The sign says "Welcome to Burlington Code Academy! Come on
              up to the third floor. If the door is locked, use the code
              12345."`
             },
 
-  'foyer': {canChangeTo: ['stairway'],
+  'foyer': {canChangeTo: ['stairway', 'sign'],
             welcomeMessage: 
             `You are in a foyer. Or maybe it's an antechamber. Or a 
             vestibule. Or an entryway. Or an atrium. Or a narthex.
             But let's forget all that fancy flatlander vocabulary,
             and just call it a foyer. In Vermont, this is pronounced
             "FO-ee-yurr". 
-            You see a stairway ahead pf you. A copy of Seven Days lies in a corner.`
+            You see a stairway ahead of you. A copy of Seven Days lies in a corner.`
             },
 
-  'stairway': {canChangeTo: ['hallway'],
+  'stairway': {canChangeTo: ['hallway', 'foyer'],
                welcomeMessage:
               `You walk to the top of a long stair way 
               and arrive at a landing. Ahead of you is a hallway`
               },
-  'hallway': {canChangeTo: ['classroom'],
+  'hallway': {canChangeTo: ['classroom', 'kitchen'],
               welcomeMessage:
               `You have entered a hallway and see the Burlington 
               Code Academy classroom entrance with a kitchen to its left`
@@ -84,41 +85,53 @@ function enterState(newState) {
 async function start() {
   console.log(rooms[currentRoom].welcomeMessage);
 
-  let answer = await ask('>_');
-  answer = inputConverter(answer) // standardizes input to lower case, trimmed, string
+  let answer = await ask('What would you like to do? \n >_');
+      answer = inputConverter(answer) // standardizes input to lower case, trimmed, string
+
   while(answer !== 'exit') {
     if (answer == 'read sign') {
       enterState('sign')
       console.log(rooms[currentRoom].welcomeMessage);
-      console.log(currentRoom)
     } 
     else if (answer == 'take sign') {
       console.log ('That would be selfish. How will other students find their way?')
     } 
     else if (answer == 'open door' || 'open') { 
       console.log ('The door is locked. There is a keypad on the door handle.')
+      start()
+      if (answer.includes('12345')) {
+        console.log('Success! The door opens. You enter the foyer and the door\nshuts behind you');
+        enterState('foyer')
+        console.log(rooms[currentRoom].welcomeMessage)
+      } else if (answer.includes('seven days') || answer.includes('take')) {
+        console.log ('You pick up the paper and leaf through it looking for comics\nand ignoring the articles, just like everbody else does');
+        player.inventory.push('seven days')
+      } else if (answer.includes('drop') || answer.includes('leaves')) {
+        console.log (`You toss the "Seven Days" to the ground.`);
+        player.inventory.pop()
+      } 
     } 
-    else if (currentRoom === 'sign' && answer === 'enter code 12345' || currentRoom === 'sign' && answer === 'key in 12345') {
-      console.log('Success! The door opens. You enter the foyer and the door\nshuts behind you');
-      enterState('foyer')
+    else if (answer.includes('stairs')) {
+      enterState('stairs')
       console.log(rooms[currentRoom].welcomeMessage);
-    } 
-    else if (answer == 'enter code 12345'|| answer == 'key in 12345'){ 
-      enterState('outside')
-      console.log(rooms[currentRoom].welcomeMessage);
     }
-    else if (answer.includes("enter code ") && !answer.includes("12345")) {
-      console.log("Bzzzzt! The door is still locked.")
+    else if (answer.includes('kitchen')) {
+      enterState('kitchen')
+      console.log(rooms[currentRoom].welcomeMessage)
+      if (answer.includes('tea')) {
+        player.inventory.push('tea')
+      } else { start() }
     }
-    else if (currentRoom === 'foyer' && answer == 'take seven days') {
-      console.log ('You pick up the paper and leaf through it looking for comics\nand ignoring the articles, just like everbody else does');
-      player.inventory.push('seven days')
+    else if (answer.includes('classroom')) {
+      enterState('classroom')
+      console.log(rooms[currentRoom].welcomeMessage)
+      if (answer.includes('leave')) {
+        enterState('hallway')
+        console.log(rooms[currentRoom].welcomeMessage)
+        start()
+      }
     }
-    else if (answer === ("drop seven days") && player.inventory.includes("seven days")){
-      console.log (`You toss the "Seven Days" to the ground.`);
-      player.inventory.pop()
-    } 
-    else if (answer === )
+    
     else { 
     console.log("Sorry, I don't understand that.");
     }
