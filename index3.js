d24a64d5944d4e0e057503172324d8364a804313
Lee Peters <lpeters0023@gmail.com>
2020-02-15 14:24:26 -0500

    added inventory check, instructions, and game end

--- index3.js
+++ index3.js
@@ -8,6 +8,7 @@ function ask(questionText) {
   });
 }
 // | - - - starter code - do not change above - - - |
+
 let currentRoom = "outside";
 
 // player object 
@@ -16,55 +17,33 @@ let player = {
   inventory: []
 }
 
-function showInventory() {
-  console.log('You are carrying, ' + player.inventory.toString())
-}
-
 let rooms = {
   'outside': {canChangeTo: ['sign'],   // outside is also known as "182 Main St."
-              welcomeMessage: `182 Main St.
-              You are standing on Main Street between Church and South Winooski.
-              There is a door here. A keypad sits on the handle.
-              On the door is a handwritten sign.
-              (If you ever want to exit, just type -     exit   )`
+              welcomeMessage: `182 Main St. You are standing on Main Street between Church and South Winooski. There is a door here. A keypad sits on the handle. On the door is a handwritten sign.
+              (If you ever want to exit, just type - exit )`
             },
-          
   'sign':   {canChangeTo: ['foyer', 'outside'],
-             welcomeMessage: `The sign says "Welcome to Burlington Code Academy! Come on
-             up to the third floor. If the door is locked, use the code
-             12345."`
+             welcomeMessage: `The sign says "Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345."`
             },
-
   'foyer': {canChangeTo: ['stairway', 'sign'],
             welcomeMessage: 
-            `You are in a foyer. Or maybe it's an antechamber. Or a 
-            vestibule. Or an entryway. Or an atrium. Or a narthex.
-            But let's forget all that fancy flatlander vocabulary,
-            and just call it a foyer. In Vermont, this is pronounced
-            "FO-ee-yurr". 
-            You see a stairway ahead of you. A copy of Seven Days lies in a corner.`
+            `You are in a foyer. Or maybe it's an antechamber. Or a vestibule. Or an entryway. Or an atrium. Or a narthex. But let's forget all that fancy flatlander vocabulary, and just call it a foyer. In Vermont, this is pronounced "FO-ee-yurr". You see a stairway ahead of you. A copy of Seven Days lies in a corner.`
             },
-
   'stairway': {canChangeTo: ['hallway', 'foyer'],
                welcomeMessage:
-              `You walk to the top of a long stair way 
-              and arrive at a landing. Ahead of you is a hallway`
+              `You walk to the top of a long stair way and arrive at a landing. Ahead of you is a hallway`
               },
   'hallway': {canChangeTo: ['classroom', 'kitchen'],
               welcomeMessage:
-              `You have entered a hallway and see the Burlington 
-              Code Academy classroom entrance with a kitchen to its left`
+              `You have entered a hallway and see the Burlington Code Academy classroom entrance with a kitchen to its left`
               },
   'classroom': {canChangeTo: ['hallway'],
               welcomeMessage:
-              `You enter the BCA classroom and find instructor Bob lecturing to 
-              an empty classroom about recursion. He's clearly stuck in an infinite loop. 
-              You notice his tea mug is empty`
+              `You enter the BCA classroom and find instructor Bob lecturing to an empty classroom about recursion. He's clearly stuck in an infinite loop. You notice his tea mug is empty`
                },
   'kitchen': {canChangeTo : ['hallway'],
               welcomeMessage:
-              `You enter the kitchen. There, you find some utensils, 
-              a coffee machine, and the ingredients to make some delicious green tea`
+              `You enter the kitchen. There, you find some utensils, a coffee machine, and the ingredients to make some delicious green tea`
               },
 };
 
@@ -81,7 +60,15 @@ function enterState(newState) {
 }
 
 // | - - - initialize game  - - - |
-
+async function directions() {
+  let input = await ask(`Welcome to zorKington, a land of many mysteries and immense beauty. Throughout the game, you will encounter many treasures. To take an item with you, simply type the name of the item. To drop an item, type 'drop_ item name'. To view your inventory, type 'inventory. Press enter to begin.`)
+  if (input.includes('')) {
+    start()
+  } else { 
+    directions() 
+  }
+}
+// | - - - begin game  - - - |
 async function start() {
   console.log(rooms[currentRoom].welcomeMessage);
 
@@ -89,15 +76,19 @@ async function start() {
       answer = inputConverter(answer) // standardizes input to lower case, trimmed, string
 
   while(answer !== 'exit') {
-    if (answer == 'read sign') {
+    if (answer === 'read sign') {
       enterState('sign')
       console.log(rooms[currentRoom].welcomeMessage);
     } 
-    else if (answer == 'take sign') {
+    if (answer.includes('inventory')) {
+      showInventory()
+      start()
+      }
+    else if (answer === 'take sign') {
       console.log ('That would be selfish. How will other students find their way?')
     } 
     else if (answer.includes('12345')) {
-        console.log('Success! The door opens. You enter the foyer and the door\nshuts behind you');
+        console.log('Success! The door opens. You enter the foyer and the door shuts behind you\n ');
         enterState('foyer')
         console.log(rooms[currentRoom].welcomeMessage)
       } 
@@ -128,6 +119,7 @@ async function start() {
       if (answer.includes('tea')) {
         player.inventory.push('tea')
       } 
+    }
 
     else if (answer.includes('class')) {
       enterState('classroom')
@@ -137,21 +129,24 @@ async function start() {
         console.log(rooms[currentRoom].welcomeMessage)
         start()
       }
+    } 
+    else if (answer.includes('class')) {
+      enterState('classroom')
+      console.log(rooms[currentRoom].welcomeMessage)
+      if (answer.includes('give bob tea')) {
+        console.log('You win the game!')
+        process.exit
+      }
     }
-    
-    else { 
-    console.log("Sorry, I don't understand that.");
+    else {
+      console.log("Sorry, I don't understand that.");
     }
-    if (answer === 'inventory' || answer === 'i') {
-    showInventory()
-    start()
-    }
-  //answer = await ask('What would you like to do? \n >_');
-  }
+  //answer = await ask('What would you like to do? \n >_');  
    process.exit();
+  }
 }
 
-start();
+directions();
 
 // | - - - process functions - do not change below  - - - |
 
@@ -159,4 +154,8 @@ start();
 function inputConverter(string) { 
   let converted = string.toString().trim().toLowerCase();
   return converted;
+}
+// func to return player inventory at a given time as a string 
+function showInventory() {
+  console.log('You are carrying, ' + player.inventory.toString())
 }
