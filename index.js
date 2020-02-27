eb78bbc081c3dade0d6217040b8cc768c75736de
Denis Poirier <D.Poirier@me.com>
2020-02-26 19:16:20 -0500

    jointly changed code per recommendations

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// project #2 Zorkington, a text adventure game - Lee and Denis 
+
 const readline = require('readline');
  
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
@@ -11,11 +13,12 @@ function ask(questionText) {
 
 let currentRoom = "outside";
 
-// player object 
+// player inventory
 let inventory = []
 
+// | - - - room definitions including messages and allowable state (location) transitions - - - |
 let rooms = {
-  'outside': {canChangeTo: ['sign', 'outside'],   // outside is also known as "182 Main St."
+  'outside': {canChangeTo: ['sign', 'outside', 'foyer'],   // outside is also known as "182 Main St."
               welcomeMessage: `182 Main St. You are standing on Main Street between Church and South Winooski. There is a door here. A keypad sits on the handle.\nOn the door is a handwritten sign.`
             },
   'sign':   {canChangeTo: ['outside', 'sign'],
@@ -56,18 +59,8 @@ function enterState(newState) {
   }
 }
 
-// | - - - initialize game  - - - |
-async function directions() {
-  let input = await ask(`Welcome to zorKington, a land of many mysteries and immense beauty. Throughout the game, you will encounter many treasures. To take an item with you, simply type the name 'take' and of the item. To drop an item, type 'drop_ item name'. To view your inventory, type 'inventory. \nIf you ever want to exit, just type 'exit'.\nPress enter to begin.`)
-  if (input.includes('')) {
-    start()
-    enterState('outside')
-  } else { 
-    directions() 
-  }
-}
 
-// | - - - begin game  - - - |
+// | - - - begin game / top to bottom flow - - - |
 
 async function start() {
   console.log(rooms[currentRoom].welcomeMessage);
@@ -80,12 +73,16 @@ async function start() {
       enterState('sign')
       console.log(rooms[currentRoom].welcomeMessage);
     } 
-    if (answer.includes('inventory')) {
-      showInventory()
+    else if (answer.includes('inventory')) {
+      showInventory() // call to show inventory function displaying 'inventory' array
       }
     else if (answer === 'take sign') {
       console.log ('That would be selfish. How will other students find their way?')
     } 
+
+    else if (answer !== '12345' && currentRoom === 'sign') {
+      console.log('Sorry, that is the wrong code, try again\n ');
+    }
     else if (answer.includes('12345')) {
         console.log('Success! The door opens. You enter the foyer and the door shuts behind you\n ');
         rooms.foyer.locked = false
@@ -94,9 +91,9 @@ async function start() {
         console.log(rooms[currentRoom].welcomeMessage)
       } 
 
-    else if (answer.includes('take seven')) {
+    else if ((answer.includes('take seven') && currentRoom === 'foyer')) {
       console.log ('You pick up the paper and leaf through it looking for comics\nand ignoring the articles, just like everbody else does.');
-      addToInventory('Seven Days')
+      addToInventory('Seven Days') 
     }   
     
     else if (answer.includes('drop seven')) {
@@ -118,7 +115,7 @@ async function start() {
       console.log(rooms[currentRoom].welcomeMessage)
     }
 
-    else if (answer.includes('take tea') || answer.includes('make tea')) {
+    else if ((answer.includes('take tea') || answer.includes('make tea')) && currentRoom === 'kitchen') {
       addToInventory('tea')
     } 
     
@@ -127,40 +124,50 @@ async function start() {
       console.log(rooms[currentRoom].welcomeMessage)
     } 
 
-    else if (answer.includes('give bob tea') || answer.includes('drop tea')) {
+    else if ((answer.includes('give bob tea') || answer.includes('drop tea')) && currentRoom === 'classroom') {
       dropFromInventory('tea')
       console.log('You win the game!')
       process.exit()
     }
-    //else {
-      //console.log("Sorry, I don't understand that.")
-    //}
+    else {
+      console.log("Sorry, I don't understand that.")
+    }
   answer = await ask('What would you like to do? \n >_') 
   }
 }
-
-
 
 directions();
 
 // | - - - process functions - do not change below  - - - |
 
+// | - - - initialize game  - - - |
+async function directions() {
+  let input = await ask(`Welcome to zorKington, a land of many mysteries and immense beauty. Throughout the game, you will encounter many treasures. To take an item with you, simply type the name 'take' and of the item. To drop an item, type 'drop_ item name'. To view your inventory, type 'inventory. \nIf you ever want to exit, just type 'exit'.\nPress enter to begin.`)
+  if (input.includes('')) {
+    start()
+    enterState('outside')
+  } else { 
+    directions() 
+  }
+}
+
 // func to standardize input returning a lower case, trimmed, string
 function inputConverter(string) { 
-  let converted = string.toString().trim().toLowerCase();
-  return converted;
+  return string.toString().trim().toLowerCase();
+
 }
 // func to return player inventory at a given time as a string 
 function showInventory() {
   console.log('You are carrying, ' + inventory.toString())
 }
-// func to add items to player's inventory
+// func to add items to player's 'inventory' array
 function addToInventory(toAdd) {
   inventory.push(toAdd)
   console.log('You have added ' + toAdd)
 }
-// func to remove 
+// func to remove items from player 'inventory' array
 function dropFromInventory(toDrop) {
   console.log('You have dropped ' + toDrop)
-  inventory.pop()
+  let dropIndex = inventory.indexOf(toDrop);
+  inventory.splice(dropIndex, 1)
 }
